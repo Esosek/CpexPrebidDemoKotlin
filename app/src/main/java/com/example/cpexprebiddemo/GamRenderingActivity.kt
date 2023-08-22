c84472317d321c2bbbbf4f1b4e6f7ac0a2e1d30b
Ales Zima <ales@cpex.cz>
2023-08-22 14:50:21 +0200

    Refactoring of GAM Rendering
    Simplified workflow with ad units
    - Added AdUnit data class
    - Added adUnits definition
    - Added dynamical AdUnits setup

--- app/src/main/java/com/example/cpexprebiddemo/GamRenderingActivity.kt
+++ app/src/main/java/com/example/cpexprebiddemo/GamRenderingActivity.kt
@@ -19,12 +19,26 @@ import com.google.android.gms.ads.admanager.AdManagerAdView
 import org.prebid.mobile.api.rendering.listeners.BannerViewListener
 import org.prebid.mobile.eventhandlers.GamBannerEventHandler
 
+data class AdUnit(
+    val pbjsConfigId: String,
+    val gamAdUnitId: String,
+    val adSize: AdSize,
+    val layoutContainerId: Int
+)
+
 class GamRenderingActivity : FragmentActivity() {
 
-    // Magnite server config
+
     companion object {
+        // Magnite server config
         val PBS_HOST = Host.RUBICON
         const val PBS_ACCOUNT_ID = "10900-mobilewrapper-0"
+
+        // Ad Units definition
+        private val adUnits = mapOf(
+            "smallRectangle" to AdUnit("10900-imp-rectangle-300-50", "/22631723832/com.example.cpexprebiddemo_small_rectangle", AdSize(300,50), R.id.smallRectangleContainer),
+            "bigRectangle" to AdUnit("10900-imp-rectangle-300-250", "/22631723832/com.example.cpexprebiddemo_big_rectangle", AdSize(300,250), R.id.bigRectangleContainer)
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,28 +46,18 @@ class GamRenderingActivity : FragmentActivity() {
         setContentView(R.layout.gam_rendering_activity) // Set the XML layout here
         initPrebidSDK()
         MobileAds.initialize(this) {}
-
-
-        val smallRectangleEventHandler = GamBannerEventHandler(this, "/22631723832/com.example.cpexprebiddemo_small_rectangle", AdSize(300,50))
-        val smallRectangle = createGamBannerAdUnit("10900-imp-rectangle-300-50", smallRectangleEventHandler)
-        val smallRectangleAdContainer = findViewById<FrameLayout>(R.id.smallRectangleContainer)
-        smallRectangleAdContainer.addView(smallRectangle)
-
-        val bigRectangleEventHandler = GamBannerEventHandler(this, "/22631723832/com.example.cpexprebiddemo_big_rectangle", AdSize(300,250))
-        val bigRectangle = createGamBannerAdUnit("10900-imp-rectangle-300-250", bigRectangleEventHandler)
-        val bigRectangleAdContainer = findViewById<FrameLayout>(R.id.bigRectangleContainer)
-        bigRectangleAdContainer.addView(bigRectangle)
+        val adUnits = setupAdUnits()
 
         // Load the ads initially
-        smallRectangle.loadAd()
-        bigRectangle.loadAd()
+        adUnits["smallRectangle"]?.loadAd()
+        adUnits["bigRectangle"]?.loadAd()
 
         // Set the "Refresh" button
         val refreshButton = findViewById<Button>(R.id.refreshButton)
         refreshButton.setOnClickListener {
             Log.d("CPEx", "Calling loadAd()")
-            smallRectangle.loadAd()
-            bigRectangle.loadAd()
+            adUnits["smallRectangle"]?.loadAd()
+            adUnits["bigRectangle"]?.loadAd()
         }
     }
 
@@ -70,7 +74,24 @@ class GamRenderingActivity : FragmentActivity() {
         PrebidMobile.setShareGeoLocation(false)
     }
 
-    private fun createGamBannerAdUnit(configId: String, eventHandler: GamBannerEventHandler): BannerView {
-        return BannerView(this, configId, eventHandler)
+    // Put together a single BannerView with GamEventHandler
+    private fun createGamBannerAdUnit(pbsConfigId: String, gamAdUnitId : String, adSize: AdSize): BannerView {
+        val eventHandler = GamBannerEventHandler(this, gamAdUnitId, adSize)
+        return BannerView(this, pbsConfigId, eventHandler)
+    }
+
+    // Setup complete ad units to a string map for better ad control
+    private fun setupAdUnits(): Map<String, BannerView> {
+        val adUnitViews = mutableMapOf<String, BannerView>()
+
+        for ((adUnitName, adUnit) in adUnits) {
+            val container = findViewById<FrameLayout>(adUnit.layoutContainerId)
+            val gamBanner = createGamBannerAdUnit(adUnit.pbjsConfigId, adUnit.gamAdUnitId, adUnit.adSize)
+
+            container.addView(gamBanner)
+            adUnitViews[adUnitName] = gamBanner
+        }
+
+        return adUnitViews
     }
 }
